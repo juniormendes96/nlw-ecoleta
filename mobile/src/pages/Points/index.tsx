faf983a830b2d545e1f47db24b46606dc5e05311
Junior <vilmarmendesjunior@hotmail.com>
2020-06-07 14:18:39 -0300

    feat: image upload and validations

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -18,6 +18,7 @@ interface Point {
   id: number;
   name: string;
   image: string;
+  image_url: string;
   latitude: number;
   longitude: number;
 }
@@ -114,7 +115,7 @@ const Points = () => {
                     <Image
                       style={styles.mapMarkerImage}
                       source={{
-                        uri: point.image
+                        uri: point.image_url
                       }}
                     />
                     <Text style={styles.mapMarkerTitle}>{point.name}</Text>
